a55a5e8f5333496da0d6da1f3a5b770666338279
Krayfaus <krayfaus@protonmail.com>
2021-04-13 19:44:13 -0300

    Added base dependencies.

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+function MainView()
+{
+  return(<h1>Hello, World!</h1>);
+}
+
+render(<MainView />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  context: __dirname,  // to find tsconfig.json automatically.
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  output: {
+    path:__dirname+ '/dist/',
+    filename: "app.bundle.js",
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          transpileOnly: true  // type checking now takes place in ForkTS plugin.
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      eslint: {
+        files: './src/**/*.{ts,tsx,js,jsx}'
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html')
+    }),
+  ]
+};
