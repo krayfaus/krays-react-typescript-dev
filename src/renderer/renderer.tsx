d76236481b268d737d40e0884b7cfdda7650c1e5
Krayfaus <krayfaus@protonmail.com>
2021-04-13 23:20:35 -0300

    Production ready Electro/React/TypeScript template!?

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -3,7 +3,14 @@ import { render } from 'react-dom';
 
 function MainView()
 {
-  return (<h1>Hello, World!</h1>);
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      We are using Node.js <span id="node-version" />
+      , Chromium <span id="chrome-version" />
+      , and Electron <span id="electron-version" />.
+    </>
+  );
 }
 
 render(<MainView />, document.getElementById('root'));
